edc2ae778317f10dc14dd9e54749dc2aff35e8a9
Cryptonoid2030 <137273973+Cryptonoid2030@users.noreply.github.com>
2025-04-03 18:06:20 -0400

    first code

--- queue.c
+++ queue.c
@@ -1,17 +1,36 @@
 #include "queue.h"
-#include "tile_game.h"
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdbool.h>
 
 void enqueue(struct queue *q, struct game_state state) {
-
+    insert_at_tail(&q->data, serialize(state));
 }
 
-struct game_state dequeue(struct queue *q) { 
-  
-  return (struct game_state){0}; 
+struct game_state dequeue(struct queue *q) {
+    return deserialize(remove_from_head(&q->data));
 }
 
 int number_of_moves(struct game_state start) {
-
-  return 0;
+    struct queue q = { .data = { .head = NULL } };
+    enqueue(&q, start);
+    
+    while (q.data.head) {
+        struct game_state cur = dequeue(&q);
+        if (serialize(cur) == serialize((struct game_state){
+            .tiles = {
+                {1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 0}
+            },
+            .empty_row = 3, .empty_col = 3, .num_steps = 0
+        })) return cur.num_steps;
+        
+        struct game_state next;
+        next = cur; move_up(&next); if (next.num_steps > cur.num_steps) enqueue(&q, next);
+        next = cur; move_down(&next); if (next.num_steps > cur.num_steps) enqueue(&q, next);
+        next = cur; move_left(&next); if (next.num_steps > cur.num_steps) enqueue(&q, next);
+        next = cur; move_right(&next); if (next.num_steps > cur.num_steps) enqueue(&q, next);
+    }
+    
+    return -1; // No solution found
 }
-
